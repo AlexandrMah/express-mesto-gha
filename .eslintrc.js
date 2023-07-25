d917aa95805cfa7240f8c3bc2aaad373d6e80214
Aleksandr Makhin <machin.alexandr@mail.ru>
2023-07-25 11:21:16 +0300

    13 ПР Ревью2 2

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,13 +23,11 @@ module.exports = {
             }
         }
     ],
-    "parser": "@typescript-eslint/parser",
     "parserOptions": {
         "ecmaVersion": "latest",
         "sourceType": "module"
     },
     "plugins": [
-        "@typescript-eslint",
         "react"
     ],
     "rules": {
