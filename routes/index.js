4b7220c94898de404c9463555967983ceed90855
Aleksandr Makhin <machin.alexandr@mail.ru>
2023-08-04 14:44:46 +0300

    Сделать все до заголовка последние штрихи3

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { celebrate, Joi, errors  } = require('celebrate');
 
 const userRouter = require('./users');
 const cardRouter = require('./cards');
@@ -7,8 +8,19 @@ const {
 } = require('../controllers/users');
 const { auth } = require('../middlewares/auth');
 
+router.post('/signup', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+    name: Joi.string().min(2).max(30),
+    avatar: Joi.string(),
+    about: Joi.string().min(2).max(30),
+  }),
+}), createUser);
+
+router.use(errors());
+
 router.post('/signin', login);
-router.post('/signup', createUser);
 
 router.use(auth);
 
@@ -17,6 +29,10 @@ router.use('/cards', cardRouter);
 
 router.use((req, res) => {
   res.status(404).send({ message: `Ресурс по адресу ${req.path} не найден` });
+});
+
+router.use((err, req, res, next) => {
+  // это обработчик ошибки
 });
 
 module.exports = router;
