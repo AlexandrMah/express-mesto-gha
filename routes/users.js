fa53b50e766a02c78de6d3174908baa6436009db
Aleksandr Makhin <machin.alexandr@mail.ru>
2023-08-04 23:22:07 +0300

    Пройти тесты 4.1

--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,15 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 const {
   getUsers, getUser, changeProfile, userInfo, changeAvatar,
 } = require('../controllers/users');
-const { celebrate, Joi, errors } = require('celebrate');
 
 router.get('/', getUsers);
 
 router.get('/me', userInfo);
 
 router.get('/:userId', celebrate({
-  body: Joi.object().keys({
+  params: Joi.object().keys({
     _id: Joi.string().required().regex(/^(?=[a-f\d]{24}$)(\d+[a-f]|[a-f]+\d)/i),
   }),
 }), getUser);
