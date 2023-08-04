e17f5f26525b37d5a7c95b745802666464017e60
Aleksandr Makhin <machin.alexandr@mail.ru>
2023-08-04 23:48:40 +0300

    Пройти тесты 5.1

--- routes/users.js
+++ routes/users.js
@@ -15,9 +15,9 @@ router.get('/:userId', celebrate({
 }), getUser);
 
 router.patch('/me', celebrate({
-  params: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    about: Joi.string().required().min(2).max(30),
   }),
 }), changeProfile);
 
