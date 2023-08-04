549a482525e211a676f5bae65d59c858b71bdf4d
Aleksandr Makhin <machin.alexandr@mail.ru>
2023-08-04 15:15:57 +0300

    Сделать все до заголовка последние штрихи4.3

--- routes/index.js
+++ routes/index.js
@@ -13,14 +13,21 @@ router.post('/signup', celebrate({
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
     name: Joi.string().min(2).max(30),
-    avatar: Joi.string(),
+    avatar: Joi.string().pattern(
+      /^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{1,6}\b([-a-zA-Z0-9\-._~:/?#[\]@!$&'()*+.;=]*)$/,
+    ),
     about: Joi.string().min(2).max(30),
   }),
 }), createUser);
 
-router.use(errors());
+router.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+  }),
+}), login);
 
-router.post('/signin', login);
+router.use(errors());
 
 router.use(auth);
 
